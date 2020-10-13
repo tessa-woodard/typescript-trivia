4506b94cb6d457e0b6f28a0971a7b328ff5107d7
Tessa Woodard <tessawoodard@outlook.com>
2020-10-12 21:58:31 -0400

    done

--- src/components/QuestionCard.tsx
+++ src/components/QuestionCard.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 
+//Types
+import { AnswerObject } from '../App'
+
+//Styles
+import { Wrapper, ButtonWrapper } from './QuestionCard.styles'
+
 type Props = {
     question: string
     answers: string[]
-    callback: any
-    userAnswer: any
+    callback: (e: React.MouseEvent<HTMLButtonElement>) => void
+    userAnswer: AnswerObject | undefined
     questionNr: number
     totalQuestions: number
 }
@@ -15,25 +21,27 @@ const QuestionCard: React.FC<Props> = ({
     callback,
     userAnswer,
     questionNr,
-    totalQuestions
+    totalQuestions,
 }) => (
-        (
+        <Wrapper>
+            <p className='number'>
+                Question: {questionNr} / {totalQuestions}
+            </p>
+            <p dangerouslySetInnerHTML={{ __html: question }} />
             <div>
-                <p className="number">
-                    Question: {questionNr} / {totalQuestions}
-                </p>
-                <p dangerouslySetInnerHTML={{ __html: question }} />
-                <div>
-                    {answers.map(answer => (
-                        <div>
-                            <button disabled={userAnswer} onClick={callback}>
-                                <span dangerouslySetInnerHTML={{ __html: answer }} />
-                            </button>
-                        </div>
-                    ))}
-                </div>
+                {answers.map((answer) => (
+                    <ButtonWrapper
+                        key={answer}
+                        correct={userAnswer?.correctAnswer === answer}
+                        userClicked={userAnswer?.answer === answer}
+                    >
+                        <button disabled={userAnswer ? true : false} value={answer} onClick={callback}>
+                            <span dangerouslySetInnerHTML={{ __html: answer }} />
+                        </button>
+                    </ButtonWrapper>
+                ))}
             </div>
-        )
+        </Wrapper>
     )
 
 export default QuestionCard
